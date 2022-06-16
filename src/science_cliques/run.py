baefc68b1c43fe2510d8d42cfb5157fe0a1bd152
Jannetty <jannetty@uw.edu>
2022-06-16 12:11:13 -0700

    Added all parameter vals to run

--- src/science_cliques/run.py
+++ src/science_cliques/run.py
@@ -81,33 +81,60 @@ if __name__ == "__main__":
     run_number = 1
     total_runs = 7200
 
+
+
     for num_neighbors in possible_number_of_neighbors:
         for number_of_individuals in possible_number_of_individuals:
             for number_of_facts in possible_number_of_facts:
                 for investigation_probability in possible_investigation_probability:
-                    for i in range(10):
-                        model = CliqueModel(
-                            number_of_individuals=number_of_individuals,
-                            number_of_neighbors=num_neighbors,
-                            num_facts=number_of_facts,
-                            investigation_probability=investigation_probability,
-                            skeptical=True,
-                        )
+                    for philosophy in range(4):
+                        if philosophy == 0:
+                            skeptical = True
+                            reid = False
+                            direct = False
+                            indirect = False
+                        if philosophy == 1:
+                            skeptical = False
+                            reid = True
+                            direct = False
+                            indirect = False
+                        if philosophy == 2:
+                            skeptical = False
+                            reid = False
+                            direct = True
+                            indirect = False
+                        if philosophy == 3:
+                            skeptical = False
+                            reid = False
+                            direct = False
+                            indirect = True
 
-                        for i in range(500):
-                            print(i)
-                            model.step()
+                        for i in range(10):
+                            model = CliqueModel(
+                                number_of_individuals=number_of_individuals,
+                                number_of_neighbors=num_neighbors,
+                                num_facts=number_of_facts,
+                                investigation_probability=investigation_probability,
+                                skeptical=skeptical,
+                                reid=reid,
+                                direct=direct,
+                                indirect=indirect,
+                            )
 
-                        # get summary statistics
-                        model.update_model_truth_false_mean_and_truth_false_total()
-                        truth_mean = model.truth_mean
-                        truth_total = model.total_truth
-                        false_mean = model.false_mean
-                        false_total = model.total_false
+                            for i in range(500):
+                                print(i)
+                                model.step()
 
-                        run_number = run_number + 1
-                        print(
-                            f"Run number: {run_number}, {total_runs - run_number} "
-                            f"runs remaining."
-                        )
-                        export_data(model)
+                            # get summary statistics
+                            model.update_model_truth_false_mean_and_truth_false_total()
+                            truth_mean = model.truth_mean
+                            truth_total = model.total_truth
+                            false_mean = model.false_mean
+                            false_total = model.total_false
+
+                            run_number = run_number + 1
+                            print(
+                                f"Run number: {run_number}, {total_runs - run_number} "
+                                f"runs remaining."
+                            )
+                            export_data(model)
